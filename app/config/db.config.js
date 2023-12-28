adfffd32ba8e18c336c1d7cf1cdc952a29f6aa0c
Vishal <vishalchauhan3549@gmail.com>
2023-12-28 16:43:05 +0530

    Updated for DB

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,13 @@
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "admin",
+//   DB: "testdb"
+// };
+
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb"
-};
+    HOST: "aws.connect.psdb.cloud",
+    USER: "dtlm5qwwcu703w7sqajs",
+    PASSWORD: "pscale_pw_yIRRHxF4AUSKCHxm3AmihBzTU2l3rK17EtOhklCxgaY",
+    DB: "test-database1",
+}
--- app/models/db.js
+++ app/models/db.js
@@ -1,11 +1,15 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const dbConfig = require("../config/db.config.js");
 
-var connection = mysql.createPool({
+var connection = mysql.createConnection({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  database: dbConfig.DB,
+  ssl: {
+    // Enable SSL/TLS
+    rejectUnauthorized: true, // Set to false if using self-signed certificates (not recommended for production)
+  },
 });
 
 module.exports = connection;
