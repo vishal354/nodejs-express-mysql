2c30577166f3565eb7872ff032a0f26678bacbb3
Vishal <vishalchauhan3549@gmail.com>
2023-12-30 22:42:12 +0530

    Connected to planetscale database

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "root",
-//   PASSWORD: "admin",
-//   DB: "testdb"
-// };
-
 module.exports = {
     HOST: "aws.connect.psdb.cloud",
-    USER: "dtlm5qwwcu703w7sqajs",
-    PASSWORD: "pscale_pw_yIRRHxF4AUSKCHxm3AmihBzTU2l3rK17EtOhklCxgaY",
+    USER: "i67l4dc0e06m9v5idt1p",
+    PASSWORD: "pscale_pw_eAToCFYkcoQKB1LSrJxTN7G1CGcwwHlCif2zpJpcKn7",
     DB: "test-database1",
 }
+
+// module.exports = {
+//     HOST: "tutorial-db1.cftutsxhh1as.us-east-1.rds.amazonaws.com",
+//     USER: "admin",
+//     PASSWORD: "admin123",
+//     DB: "test_db1",
+// }
